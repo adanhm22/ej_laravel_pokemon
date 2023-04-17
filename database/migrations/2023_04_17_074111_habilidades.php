670be447609cf8e58c1d66c2835c0faf8a9d5a22
Jose <antoniojosee76@gmail.com>
2023-04-17 09:49:40 +0200

    migración

--- database/migrations/2023_04_17_074111_habilidades.php
+++ database/migrations/2023_04_17_074111_habilidades.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('habilidades', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre',50);
             $table->timestamps();
+            $table->unsignedBigInteger('fk_id_habilidades');
+            $table->foreign('fk_id_habilidades')
+                ->references('id')
+                ->on('pokemons')
+                ->onDelte('cascade')
+                ->onUpdate('cascade');
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('habilidades');
     }
 };
